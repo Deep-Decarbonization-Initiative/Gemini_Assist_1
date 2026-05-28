c192bbfc3af0f545b14aea142c9d19a51ec97d69
Deep-Decarbonization-Initiative <jmyers@ucsd.edu>
2026-05-28 23:29:58 +0000

    Pushing event labels higher

--- streamlit_app.py
+++ streamlit_app.py
@@ -384,7 +384,7 @@ if df_research is not None:
                 event_text = alt.Chart(event_markers_filtered).mark_text(
                     align='left',
                     baseline='middle',
-                    dx=100,
+                    dx=300,
                     dy=5,
                     angle=270,
                     color='#666666',
